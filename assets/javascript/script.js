b4f97e47005288dfc292f6a68aa69f57ce743f1b
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-02-20 19:16:25 +0000

    basic css and mapping of potential functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,49 @@
+DOM ready:
+
+event.Listeners:
+
+button clicks
+
+enter button
+
+
+let allTasks = [
+     {
+task: "wash dishes",
+date: new Date(),
+priorityStatus: red, amber, green
+taskCompleted: true/false
+    },
+
+    {
+task: "",
+date:new Date(),
+priorityStatus: red, amber, green
+taskCompleted: true/false
+    },
+];
+
+/**Adds tasks to the list */
+function addTask () {
+
+}
+
+/**Changes the priority of the tasks on the list */
+function changePriorityStatus () {
+
+}
+
+/**Sorts tasks by date and puts them in the appropriate list */
+function dateSort () {
+
+}
+
+/**Edits each task */
+function editTask () {
+
+}
+
+/**remove the task */
+function removeTask () {
+
+}
