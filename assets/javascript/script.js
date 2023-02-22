5bcefc3b7c7486a4ace331405e4fae3fad4c260c
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-02-22 16:57:43 +0000

    add new row attempt

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,49 +1,100 @@
-DOM ready:
-
-event.Listeners:
-
-button clicks
-
-enter button
+// Wait for the DOM to finish loading before running the game
+// Get the Add button element and add event listeners to it
 
 
-let allTasks = [
-     {
-task: "wash dishes",
-date: new Date(),
-priorityStatus: red, amber, green
-taskCompleted: true/false
-    },
+document.addEventListener("DOMContentLoaded", function () {
+    let addButton = document.getElementById("add-button");
 
-    {
-task: "",
-date:new Date(),
-priorityStatus: red, amber, green
-taskCompleted: true/false
-    },
-];
+    addButton.addEventListener("click", createTask)
+})
+
+
+const date = new Date();
+const todayDay = date.getDate();
+const todayMonth = date.getMonth();
+const todayYear = date.getFullYear();
+
+let allTasks = [];
+let todaysTasks = [];
+let thisWeeksTasks = [];
+let futureTasks = [];
+
 
 /**Adds tasks to the list */
-function addTask () {
+function createTask() {
+    let taskText = document.getElementById("task-text").value;
+    let taskDate = document.getElementById("date-input").value;
+    let isImportant = document.getElementById('important').value;
+
+    let newTask = {
+        task: taskText,
+        date: taskDate,
+        taskImportant: isImportant
+    }
+
+    allTasks.push(newTask);
+    putInTheTable();
+}
+
+
+function putInTheTable() {
+
+    let task = allTasks.slice(-1)[0];
+    var taskHtml = `<tr>
+    <td>${task.task}</td>
+    <td>${task.date}</td>
+    <td>${task.taskImportant}</td>
+    </tr>`;
+
+    let taskDate = new Date(task.date);
+ 
+    let taskDay = taskDate.getDate();
+    let taskMonth = taskDate.getMonth();
+    let taskYear = taskDate.getFullYear();
+
+    if (
+        (todayDay == taskDay) &&
+        (todayMonth == taskMonth) &&
+        (todayYear == taskYear)
+    ) {
+        alert('help me god');
+        let tableBodyContents = document.getElementById("today-table").innerHTML;
+        alert(tableBodyContents);
+        tableBodyContents = tableBodyContents + taskHtml;
+        document.getElementById("today-table").innerHTML = tableBodyContents;
+    }
+}
+
+
+
+
+//     alert(date);
+
+
+
+
+
+//     // This arrangement can be altered based on how we want the date's format to appear.
+//     let currentDate = `${day}-${month}-${year}`;
+//     console.log(currentDate); // "17-6-2022"
+
+//     document.getElementById("today-table").innerHTML = taskhtml;
+// }
+
+
+
+function changePriorityStatus() {
 
 }
 
-/**Changes the priority of the tasks on the list */
-function changePriorityStatus () {
 
-}
-
-/**Sorts tasks by date and puts them in the appropriate list */
-function dateSort () {
-
-}
 
 /**Edits each task */
-function editTask () {
+function editTask() {
 
 }
 
 /**remove the task */
-function removeTask () {
+function removeTask() {
 
 }
